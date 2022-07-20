401fd7131731eae6fe057203494d7bcce2269747
Zhent <jdcaballeror@correo.udistrital.edu.co>
2022-07-19 23:51:23 -0500

    Component Column

---
+++ vue-trello/src/components/Column.vue
@@ -0,0 +1,48 @@
+<template>
+  <section>
+    <header>
+      <h3>{{ name }}</h3>
+    </header>
+  </section>
+</template>
+
+<script>
+export default {
+  name: 'column',
+  props: {
+    listId: String,
+    name: String
+  }
+}
+</script>
+
+<style lang="scss" scoped>
+  section {
+    box-sizing: border-box;
+    background-color: #eceff1;
+    border-radius: 3px;
+    box-shadow: 0 0 0 0.5px rgba(49,49,93,.03),
+      0 2px 5px 0 rgba(49,49,93,.1),
+      0 1px 2px 0 rgba(0,0,0,.08);
+    margin: 1rem;
+    padding: 1rem;
+    width: 100%;
+    @media screen and (min-width: 600px) {
+      width: calc(50% - 2rem - 2px);
+    }
+    @media screen and (min-width: 1024px) {
+      width: calc(70% - 2rem - 2px);
+    }
+    @media screen and (min-width: 1200px) {
+      width: calc(60% - 2rem - 2px);
+    }
+  }
+  header {
+    color: #37474f;
+    margin: 0;
+    padding-bottom: 1rem;
+  }
+  h3 {
+    margin: 0;
+  }
+</style>
--- vue-trello/src/components/views/Board.vue
+++ vue-trello/src/components/views/Board.vue
@@ -1,22 +1,100 @@
 <template>
   <section>
     <h3><span>Mis Paneles</span> > {{ name }}</h3>
-    <div class="">...</div>
+    <input
+    type="text"
+    placeholder="Añade una lista"
+    v-model="listName"
+    @keyup.enter="add()"
+    >
+    <div class="container">
+      <column
+        v-for="(list, index) in boardList"
+        :key="index"
+        :listId="list.id"
+        :name="list.name"
+      >
+      </column>
+    </div>
   </section>
 </template>
 
 <script>
+import Column from '@/components/Column'
+
 export default {
   name: 'board-view',
-  components: {},
+  components: { Column },
 
   props: {
     name: String,
     id: String
+  },
+
+  data () {
+    return {
+      listName: '',
+      boardList: [
+        { id: '1', name: 'Todo' },
+        { id: '2', name: 'Doing' }
+      ]
+    }
+  },
+
+  methods: {
+    add () {
+      this.boardList.push({ name: this.listName })
+    }
   }
 }
 </script>
 
-<style>
+<style lang="scss" scoped>
+  h3 {
+    color: #34474F;
+    text-align: left;
+    margin: 1.5rem;
 
+    span {
+      color: #546E7A;
+    }
+  }
+
+  section {
+    text-align: left;
+    margin: 15px;
+  }
+
+  .container {
+    box-sizing: border-box;
+    align-items: flex-start;
+    display: flex;
+    flex-direction: row;
+    flex-wrap: wrap;
+    justify-content: flex-start;
+  }
+
+  input {
+    background-color: #607d8b;
+    border: 0;
+    border-radius: 3px;
+    box-shadow: 0 0 0 0.5px rgba(49,49,93,.03),
+      0 2px 5px 0 rgba(49,49,93,.1),
+      0 1px 2px 0 rgba(0,0,0,.08);
+    color: #607d8b;
+    font-size: 1.2rem;
+    margin: 0 1rem;
+    outline: 0;
+    padding: 1rem;
+    transition: all 600ms ease;
+
+    &:active,
+    &:focus {
+      background-color: #fafafa;
+    }
+
+    &::placeholder {
+      color: #fafafa;
+    }
+  }
 </style>
